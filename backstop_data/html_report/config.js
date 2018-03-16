1235d45c433483cdacbbbfcc97aea360e164fa76
Gabriel de Oliveira Barbosa <gabriel.oliveira@bizcommerce.com.br>
2018-03-16 09:29:34 -0300

    Add references

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,234 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homepage_0_viewport_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Homepage_0_viewport_0_desktop.png",
+        "selector": "viewport",
+        "fileName": "backstop_default_Homepage_0_viewport_0_desktop.png",
+        "label": "Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homepage_0_viewport_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Homepage_0_viewport_1_iphone.png",
+        "selector": "viewport",
+        "fileName": "backstop_default_Homepage_0_viewport_1_iphone.png",
+        "label": "Homepage",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Header_0_header-container_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Header_0_header-container_0_desktop.png",
+        "selector": ".header-container",
+        "fileName": "backstop_default_Header_0_header-container_0_desktop.png",
+        "label": "Header",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Header_0_header-container_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Header_0_header-container_1_iphone.png",
+        "selector": ".header-container",
+        "fileName": "backstop_default_Header_0_header-container_1_iphone.png",
+        "label": "Header",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Footer_0_footer-container_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Footer_0_footer-container_0_desktop.png",
+        "selector": ".footer-container",
+        "fileName": "backstop_default_Footer_0_footer-container_0_desktop.png",
+        "label": "Footer",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Footer_0_footer-container_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Footer_0_footer-container_1_iphone.png",
+        "selector": ".footer-container",
+        "fileName": "backstop_default_Footer_0_footer-container_1_iphone.png",
+        "label": "Footer",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Produto_0_main-container_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Produto_0_main-container_0_desktop.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Produto_0_main-container_0_desktop.png",
+        "label": "Produto",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Produto_0_main-container_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Produto_0_main-container_1_iphone.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Produto_0_main-container_1_iphone.png",
+        "label": "Produto",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Carrinho_0_main-container_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Carrinho_0_main-container_0_desktop.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Carrinho_0_main-container_0_desktop.png",
+        "label": "Carrinho",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Carrinho_0_main-container_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Carrinho_0_main-container_1_iphone.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Carrinho_0_main-container_1_iphone.png",
+        "label": "Carrinho",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Catalogo_0_main-container_0_desktop.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Catalogo_0_main-container_0_desktop.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Catalogo_0_main-container_0_desktop.png",
+        "label": "Catalogo",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Catalogo_0_main-container_1_iphone.png",
+        "test": "../bitmaps_test/20180316-092727/backstop_default_Catalogo_0_main-container_1_iphone.png",
+        "selector": ".main-container",
+        "fileName": "backstop_default_Catalogo_0_main-container_1_iphone.png",
+        "label": "Catalogo",
+        "misMatchThreshold": 0.1,
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
